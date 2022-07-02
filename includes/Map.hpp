50462644defbd61c0e1f2a1c667c947fabc06d12
Lucas ADAWI <ladawi@e1r12p23.42.fr>
2022-07-02 15:22:02 +0200

    ++

---
+++ includes/Map.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Map.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ladawi <ladawi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/02 14:11:05 by ladawi            #+#    #+#             */
+/*   Updated: 2022/07/02 14:23:23 by ladawi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	MAP_HPP
+# define MAP_HPP
+
+namespace ft {
+
+template < class Key,                                     // map::key_type
+			class T,                                       // map::mapped_type
+			class Compare = less<Key>,                     // map::key_compare
+			class Alloc = allocator<pair<const Key,T> >    // map::allocator_type
+			>
+class map {
+			
+			
+			
+};
+
+}
+
+#endif
